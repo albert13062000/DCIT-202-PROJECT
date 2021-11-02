b55b27bf62fb568150d815b18c99037f7ddc01db
Albert Ankomah <aadodoo003@st.ug.edu.gh>
2021-11-02 15:57:49 +0000

    landing page

--- App.js
+++ App.js
@@ -1,21 +1,56 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
+  // const [flag,setflag] = useState(true)
+  // const [state, setstate] = useState(initialState)
+  // useEffect(() => {
+  //   effect
+  //   return () => {
+  //     cleanup
+  //   }
+  // }, [input])
+  
+    
+  
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+    
+    <View style={styles.container2}>
+        <Text>ALBERTO AUTO DEALERSHIP</Text>
+    </View>
+
+      
+    <View style={styles.container3}>
+      <Text>This is my shopping app</Text>
+      <Text>hellow</Text>
+        
+    </View>
+
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
+  const styles = StyleSheet.create({
+  container:{
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: "#7CA1B4",
+
+},
+  container2:{
+    paddingTop: 30,
+    paddingLeft: 10,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+},
+
+  container3: {
+    padding: 10,
+    alignItems: "center",
+
+    
   },
+  
 });
